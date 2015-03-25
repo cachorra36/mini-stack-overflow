57d54823afbdcd7bbc94fd86c436a0a3f7b9ba23
Naranjita Golden <naranja49@gmail.com>
2015-03-25 16:15:03 -0500

    Begining the second realease_ form helpers_new_create methods

--- makeit-overflow/app/controllers/questions_controller.rb
+++ makeit-overflow/app/controllers/questions_controller.rb
@@ -2,6 +2,7 @@ class QuestionsController < ApplicationController
 
 	def index
 		@questions = Question.all	
+		@question = Question.new
 	end
 
 	def show
@@ -9,4 +10,25 @@ class QuestionsController < ApplicationController
 		@question = Question.find(params[:id]) 
 		@answers = @question.answers
 	end
+
+	def new
+		@question = Question.new
+
+		
+	end
+
+	def create
+		@question = Question.new (question_params)
+		@question.save
+		redirect_to root_path
+		
+	end
+
+private
+
+	def question_params
+
+		params.require(:question).permit(:title, :content)
+	end
+
 end
