b80be66eb7a28d3113cbd3ff36459439b6d5019a
German Hernandez <muygrafico@gmail.com>
2015-03-25 13:31:45 -0500

    Finished CRUD in the controller Questions | added a partial called _form.html.erb | use_form partial inside new and edit views | all CRUD views for questions are working

--- miniStackoverflow/app/controllers/questions_controller.rb
+++ miniStackoverflow/app/controllers/questions_controller.rb
@@ -1,10 +1,13 @@
 class QuestionsController < ApplicationController
+  before_action :fetch_question , only: [:show, :edit, :update, :destroy]
+
 
   def index
     @questions = Question.all
   end
 
   def show
+    # @question = Question.find(params[:id])
   end
 
   def new
@@ -12,15 +15,37 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    # @question = Question.find(params[:id])
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      redirect_to 'new'
+    end
   end
 
   def update
+    # @question = Question.find(params[:id])
+    @question.update(question_params)
+    redirect_to @question
   end
 
   def destroy
+    # @question = Question.find(params[:id])
+    @question.destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content, :user)
+  end
+
+  def fetch_question
+    @question = Question.find(params[:id])
   end
 
 end
