f1be03746e19db6743a37d4c0ca576f9549cd0f6
German Hernandez <muygrafico@gmail.com>
2015-03-25 11:19:54 -0500

    the index of question display all the questions | created a form for new questions | defined in the route file all the CRUD of questions using resources | modified the controller within the new method to use in the form in the new.html.etb view (in quesions)

--- miniStackoverflow/app/controllers/questions_controller.rb
+++ miniStackoverflow/app/controllers/questions_controller.rb
@@ -1,12 +1,14 @@
 class QuestionsController < ApplicationController
 
   def index
+    @questions = Question.all
   end
 
   def show
   end
 
   def new
+    @question = Question.new
   end
 
   def edit
