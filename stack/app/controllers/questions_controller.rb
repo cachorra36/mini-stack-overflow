f629b459fc06958b1504ce2cade6d0600611bd3f
Alejandro Escobar <alejoescobac@gmail.com>
2015-03-25 10:44:45 -0500

    first commit

---
+++ stack/app/controllers/questions_controller.rb
@@ -0,0 +1,30 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def upvote
+  end
+
+  def downvote
+  end
+end
