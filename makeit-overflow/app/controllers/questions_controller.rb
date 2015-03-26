b0e97ed55a4fbf55a16d01ab445410dc05d09a51
Diego Fernando Urrea <Diego@Diegos-MacBook-Pro.local>
2015-03-26 10:25:29 -0500

    Finish CRUD for Questions

--- makeit-overflow/app/controllers/questions_controller.rb
+++ makeit-overflow/app/controllers/questions_controller.rb
@@ -13,16 +13,35 @@ class QuestionsController < ApplicationController
 
 	def new
 		@question = Question.new
-
-		
 	end
 
 	def create
 		@question = Question.new (question_params)
 		@question.save
 		redirect_to root_path
-		
 	end
+
+	def edit
+		@question = Question.find(params[:id])
+	end
+
+	def update
+    @question = Question.find(params[:id])
+    	if @question.update(question_params)
+      	redirect_to root_path
+    	else
+      	render 'update'
+    	end
+  	end
+
+  	def destroy
+  		@question = Question.find(params[:id])
+  		@question.destroy
+
+  		redirect_to root_path
+  	end
+
+
 
 private
 
