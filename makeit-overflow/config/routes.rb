30db403b342a323d1f17c6e1f2ed50a442f49614
Naranjita Golden <naranja49@gmail.com>
2015-03-27 11:02:04 -0500

    Upvote and Downvote without anidations and glyphicon in buttons

--- makeit-overflow/config/routes.rb
+++ makeit-overflow/config/routes.rb
@@ -5,14 +5,13 @@ Rails.application.routes.draw do
 	resources :questions do
 		member do
 			post 'upvote'
-			post 'downvote'
-		end
-
-		resources :answers do
-			member do
-				post 'upvote'
-				post 'downvote'
-			end
+			delete 'downvote'
 		end
 	end
+	resources :answers do
+		member do
+			post 'upvote'
+			delete 'downvote'
+		end
+
 end
